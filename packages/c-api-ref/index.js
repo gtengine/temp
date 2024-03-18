a217f44b87879e79d3cde6e41e88736581589b06
gtengine <prierkt@gmail.com>
2024-03-18 16:09:00 +0900

    a

--- packages/c-api-ref/index.js
+++ packages/c-api-ref/index.js
@@ -1,8 +1,8 @@
 const ffi = require("ffi-napi");
 
 const func = ffi.Library("./dlls/Mocha.dll", {
-  API_Add: ["int", ["int", "int"]],
+  API_Add: ["uint32", []],
 });
 
-const result = func.API_Add(10, 10);
+const result = func.API_Add();
 console.log("result by using dll: ", result);
